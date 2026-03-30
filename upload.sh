0ed425a25a6f4e63838ca18bec7fe70eb0859dae
75efb6 <71222525+75efb6@users.noreply.github.com>
2026-03-30 22:58:55 +0200

    Fix upload script

--- upload.sh
+++ upload.sh
@@ -4,7 +4,6 @@ set -e  # Stop on first error
 
 source .venv/bin/activate
 echo "🚀 Uploading package to PyPI..."
-cd src/
 pip install twine
 python -m twine upload dist/*
 echo "✅ Upload complete!"
